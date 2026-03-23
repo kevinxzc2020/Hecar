6646d146efd7df9613009b311a19cedadd3ad9f7
Kevinxzc2010 <zhichenxuucf@gmail.com>
2026-03-23 14:35:13 -0400

    created three new files

---
+++ app/list.tsx
@@ -0,0 +1,20 @@
+// list.tsx
+import { StyleSheet, Text, View } from "react-native";
+
+export default function ListScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>列表</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0F172A",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: { color: "#F8FAFC", fontSize: 24 },
+});
